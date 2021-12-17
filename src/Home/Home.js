0c42125ecc3e4c7bba636bac62a010c8beb7025b
Taylor <taylorpollioverturf@gmail.com>
2021-12-16 17:05:36 -0800

    create homepage skeleton, update dummy data

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,15 +1,29 @@
 import React from 'react';
+import { yiddishword } from '../WordData/Data';
 // import { Link } from "react-router-dom";
 import '../App.css';
+import './Home.css';
 
 function Home() {
 
+    // Random Number Variables
+    const randomword = Math.floor(Math.random() * 5)
+
     return (
         <>
-            <h1>This is the homepage.</h1>
+        <header>
+            <p className="my-bubbe-says-hebrew">זאָגט מײַן באָבע</p>
+            <span><i class="fas fa-bars"></i></span>
+        </header>
+        <div className="container">
+            <h1 className='title'>My bubbe says <strong className="bold-text">{yiddishword[randomword].word}</strong>.</h1>
+            <h2>[{yiddishword[randomword].phonetic_spelling}]</h2>
+            <h2>{yiddishword[randomword].definition}</h2>
+            <h3><em>{yiddishword[randomword].example_sentence}</em></h3>
 
+        </div>
             <footer className="footer">
-            <h5>Designed & Coded with &#9829; by Taylor Overturf</h5>
+            <h5>Designed & Coded with <span className="hebrew">חוצפה</span> by Taylor Overturf</h5>
             <h6>Seattle, Washington&nbsp; | &nbsp;December, 2021</h6>
             </footer>
         </>
--- src/WordData/Data.js
+++ src/WordData/Data.js
@@ -1,18 +1,50 @@
-export const worddata = [
+export const yiddishword = [
     {
-        id: 1,
+        id: 0,
         word: "verklempt",
-        phonetic_spelling: "fur-clem-tuh",
+        phonetic_spelling: "fur-klem-tuh",
         definition: "overwhelmed",
         example_sentence: "My bubbe was a little verklempt when she heard the news: her only daughter was to marry a doctor.",
         bubbe: "Gussie Ratner"
     },
     {
-        id: 2,
+        id: 1,
         word: "zadie",
         phonetic_spelling: "zay-dee",
         definition: "grandfather",
         example_sentence: "Even though Zadie can't hear very well anymore, somehow he still knows when I'm trying to steal a bite of his precious gefilte fish out of the jar.",
+        bubbe: "Gussie Ratner"
+    },
+    {
+        id: 2,
+        word: "chutzpah",
+        phonetic_spelling: "hoots-puh",
+        definition: "confidence, audacity, nerve",
+        example_sentence: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        bubbe: "Gussie Ratner"
+    },
+    {
+        id: 3,
+        word: "keppie",
+        phonetic_spelling: "kep-pee",
+        definition: "head, forehead",
+        example_sentence: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        bubbe: "Gussie Ratner"
+    },
+    {
+        id: 4,
+        word: "meshuggeneh",
+        phonetic_spelling: "meh-shug-en-uh",
+        definition: "crazy, insane",
+        example_sentence: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        bubbe: "Gussie Ratner"
+    },
+    {
+        id: 5,
+        word: "schmatte",
+        phonetic_spelling: "shmah-tuh",
+        definition: "rag, tattered cloth, beloved blanket",
+        example_sentence: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         bubbe: "Gussie Ratner"
     }
 ]
