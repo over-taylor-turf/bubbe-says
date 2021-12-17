ba3fbd12e00a0379c1c4d1f4f56e4864723b0958
Taylor <taylorpollioverturf@gmail.com>
2021-12-17 10:34:44 -0800

    add submit page

--- src/Submit/Submit.js
+++ src/Submit/Submit.js
@@ -1,36 +1,52 @@
 import React from 'react';
 // import { Link } from "react-router-dom";
 import '../App.css';
+import './Submit.css';
 
 function Submit() {
 
+    const SubForm = () => {
+        fetch("https://api.apispreadsheets.com/data/POU9eZNdrU0j2Xc1/", {
+            method: "POST",
+            body: JSON.stringify({"data": {"email":"","word":"","definition":"","example":"","bubbe":""}}),
+        }).then(res =>{
+            if (res.status === 201){
+                // SUCCESS
+                alert('Thank you for submitting a word to My Bubbe Says!')
+            }
+            else{
+                // ERROR
+                alert('There was an error. Please try again!')
+            }
+        })
+    }
+
     return (
         <>
-            <form>
+            <form id="myForm">
                 
                 <label for="email">Your Email Address</label>
-                <input type="text" name="email" placeholder="bubbe@bubbe.com"/>
+                <input type="email" name="email" placeholder="bubbe@bubbe.com"/>
                 <br/>
 
-                <label for="email">Proposed Word</label>
+                <label for="word">Proposed Word</label>
                 <input type="text" name="word" placeholder="schvitz"/>
                 <br/>
 
-                <label for="email">Definition</label>
-                <input type="text" name="definition" placeholder="Sweating profusely."/>
+                <label for="definition">Definition</label>
+                <textarea type="text" name="definition" placeholder="Sweating profusely."/>
                 <br/>
 
-                <label for="email">Example Sentence</label>
-                <input type="text" name="email" placeholder="I was schvitzing so bad it looked like I dumped a bucket of water over my head."/>
+                <label for="example">Example Sentence</label>
+                <textarea type="text" name="example" placeholder="I was schvitzing so bad it looked like I dumped a bucket of water over my head."/>
                 <br/>
 
-                <label for="email">Bubbe's Name</label>
-                <input type="text" name="email" placeholder="Bubbe Gussie"/>
+                <label for="bubbe">Bubbe's Name</label>
+                <input type="text" name="bubbe" placeholder="Bubbe Gussie"/>
                 <br/>
-
-                <input type="submit" value="Submit"></input>
-
             </form>
+            <button onClick={() => {SubForm()}}>Submit</button>
+
         </>
 
         );
