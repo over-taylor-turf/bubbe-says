2086ccb33bd2d504b9aa58a7b2fffe9c036626fe
Taylor <taylorpollioverturf@gmail.com>
2021-12-18 14:50:21 -0800

    homepage reload every 5 seconds functionality

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { yiddishword } from '../WordData/Data';
 // import { Link } from "react-router-dom";
 import '../App.css';
@@ -7,28 +7,33 @@ import Footer from '../Footer/Footer';
 import Menu from '../NavBar/NavBar';
 
 function Home() {
+    
+    // Refresh the page every 5 seconds.
+    setTimeout(() => {
+        window.location.reload();
+      }, 5000)
 
     // Random Number Variables
-    const randomword = Math.floor(Math.random() * 10)
+    const random = Math.floor(Math.random() * 10)
 
     return (
         <>
         <header>
 
             <Menu/>
-
         </header>
+
         <div className="container">
-            <h1 className='title'>My bubbe says <strong className="bold-text">{yiddishword[randomword].word}</strong>.</h1>
+            <h1 className='title'>My bubbe says <strong className="bold-text">{yiddishword[random].word}</strong>.</h1>
             <br/>
             <div id='grid-container'>
             <img id='nanny' src='https://res.cloudinary.com/overturf/image/upload/v1639786175/IMG-5145_1_vks3ih.jpg' alt='older woman in teal'/>
             <div id='main-text'>
-            <h2>[{yiddishword[randomword].phonetic_spelling}]</h2>
+            <h2>[{yiddishword[random].phonetic_spelling}]</h2>
             <br />
-            <h2>{yiddishword[randomword].definition}</h2>
+            <h2>{yiddishword[random].definition}</h2>
             <br />
-            <h3><em>{yiddishword[randomword].example_sentence}</em></h3>
+            <h3><em>{yiddishword[random].example_sentence}</em></h3>
             </div>
             </div>
         </div>
