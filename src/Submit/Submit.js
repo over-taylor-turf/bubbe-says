9e51ade158c58d9a5e06e3984feead27dc41e1cc
Taylor <taylorpollioverturf@gmail.com>
2021-12-18 19:02:12 -0800

    basic styles for submission form

--- src/Submit/Submit.js
+++ src/Submit/Submit.js
@@ -26,25 +26,29 @@ function Submit() {
     return (
         <>
             <Menu/>
+
+            <h1 className='submit-title'>What does your bubbe say?</h1>
+            <h2>Don't see your family's favorite Yiddish word or phrase? Please take a moment to submit it to our archives!</h2>
+            <br/>
             <form id="myForm">
                 
-                <label for="email">Your Email Address</label>
+                <label for="email">Your Email Address</label><br/>
                 <input type="email" name="email" placeholder="bubbe@bubbe.com"/>
                 <br/>
 
-                <label for="word">Proposed Word</label>
+                <label for="word">Proposed Word</label><br/>
                 <input type="text" name="word" placeholder="schvitz"/>
                 <br/>
 
-                <label for="definition">Definition</label>
+                <label for="definition">Definition</label><br/>
                 <textarea type="text" name="definition" placeholder="Sweating profusely."/>
                 <br/>
 
-                <label for="example">Example Sentence</label>
+                <label for="example">Example Sentence</label><br/>
                 <textarea type="text" name="example" placeholder="I was schvitzing so bad it looked like I dumped a bucket of water over my head."/>
                 <br/>
 
-                <label for="bubbe">Bubbe's Name</label>
+                <label for="bubbe">Your Bubbe's Name</label><br/>
                 <input type="text" name="bubbe" placeholder="Bubbe Gussie"/>
                 <br/>
             </form>
