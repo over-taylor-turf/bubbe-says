c285d997a94d1b887071abb7c38f4e3048e62913
Taylor <taylorpollioverturf@gmail.com>
2021-12-21 11:04:11 -0800

    update game page

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -32,35 +32,39 @@ function Game() {
         if (headsortails === 0) {
             return(
                 <>
-                <h2>The Yiddish word for: "{yiddishword[random2].definition}."</h2>
+                <h2 className="game-intro">The Yiddish word for: "{yiddishword[random2].definition}."</h2>
                 <br/>
                 <p>Is it {yiddishword[random1].word}?<br/>
                 Or, maybe... {yiddishword[random2].word}?<br/>
                 Perhaps even, {yiddishword[random3].word}?</p>
                 <br/>
+                <div className="button-wrapper">
                 <button className="game-button1" onClick={() => handleCorrectGuess()}>{yiddishword[random2].word}</button>
                 <button className="game-button2" onClick={() => handleWrongGuess()}>{yiddishword[random1].word}</button>
                 <button className="game-button3" onClick={() => handleWrongGuess()}>{yiddishword[random3].word}</button>
+                </div>
                 </>
             )
         } else if (headsortails === 1) {
             return(
                 <>
-                <h2>The Yiddish word for: "{yiddishword[random2].definition}."</h2>
+                <h2 className="game-intro">The Yiddish word for: "{yiddishword[random2].definition}."</h2>
                 <br/>
                 <p>Is it {yiddishword[random2].word}?<br/>
                 Or, maybe... {yiddishword[random1].word}?<br/>
                 Perhaps even, {yiddishword[random3].word}?</p>
                 <br/>
+                <div className="button-wrapper">
                 <button className="game-button1" onClick={() => handleWrongGuess()}>{yiddishword[random3].word}</button>
                 <button className="game-button2" onClick={() => handleCorrectGuess()}>{yiddishword[random2].word}</button>
                 <button className="game-button3" onClick={() => handleWrongGuess()}>{yiddishword[random1].word}</button>
+                </div>
                 </>
             )
         } else {
             return(
                 <>
-                <h2>The Yiddish word for: "{yiddishword[random2].definition}."</h2>
+                <h2 className="game-intro">The Yiddish word for: "{yiddishword[random2].definition}."</h2>
                 <br/>
                 <p>Is it {yiddishword[random3].word}?<br/>
                 Or, maybe... {yiddishword[random2].word}?<br/>
